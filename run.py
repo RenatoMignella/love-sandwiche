e30b93482b01426b9f9d90a96c5d3b180e3db1f9
Renato Mignella <renatoferigato@gmail.com>
2022-06-06 00:28:52 +0100

    add validation to check 6 numbers

--- run.py
+++ run.py
@@ -30,7 +30,23 @@ def get_sales_data():
     print("Examples: 10,20,30,40,50,60\n")
     
     data_str = input("enter your data here ")
-    print(f"the data proviced is {data_str}")
+   
+    sales_data = data_str.split(",")
+    validte_data(sales_data)
+    
+def validte_data(values):
+    """
+   Inside the try, converts all st
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exacly 6 values required, you provided{len(values)}"
+            )
+    except ValueError as e:
+        print(f"invalid data: {e}, please try again .")        
+    
+        
     
 get_sales_data()    
     
